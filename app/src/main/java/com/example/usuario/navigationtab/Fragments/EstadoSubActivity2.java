a716d2a7cf0f42c0a96b832c6d506d7f00346b50
manuelpt4940 <36642986+manuelpt4940@users.noreply.github.com>
2018-08-06 09:55:48 -0500

    implementation of two subactivities

--- app/src/main/java/com/example/usuario/navigationtab/Fragments/EstadoSubActivity2.java
+++ app/src/main/java/com/example/usuario/navigationtab/Fragments/EstadoSubActivity2.java
@@ -1,25 +1,21 @@
 package com.example.usuario.navigationtab.Fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.usuario.navigationtab.MainActivity;
 import com.example.usuario.navigationtab.R;
 
-public class EstadoSubActivity1 extends AppCompatActivity {
+public class EstadoSubActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_estado_sub1);
-        Button b_nacti = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_estado_sub2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,32 +27,17 @@ public class EstadoSubActivity1 extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        b_nacti.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in = new Intent(getBaseContext(),EstadoSubActivity2.class);
-                in.putExtra("SubActivity","Nueva Pestaña2");
-                startActivity(in);
-            }
-        });
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null){
             if (bundle.getString("SubActivity") != null){
                 Toast.makeText(getApplicationContext(), "data: "
-                        + bundle.getString("SubActivity"),
+                                + bundle.getString("SubActivity"),
                         Toast.LENGTH_SHORT).show();
             }
         }
-
-
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
+
 }
